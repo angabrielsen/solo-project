ef77a7f3cc18c957316fae7d4a7bc077d91e8488
Ashton Gabrielsen <ashton.gabrielsen@i28332-IS3.local>
2016-10-04 13:16:44 -0600

    formatting code

--- Main.elm
+++ Main.elm
@@ -19,58 +19,67 @@ import Material.Icon as Icon
 
 main : Program Never
 main =
-    App.program
-        { init = ( model, Cmd.none )
-        , view = view
-        , subscriptions = always Sub.none
-        , update = update
-        }
+  App.program
+    { init = ( model, Cmd.none )
+    , view = view
+    , subscriptions = always Sub.none
+    , update = update
+    }
 
 -- MODEL
 
 type alias Model =
-    { mdl : Material.Model
-    , selectedTab : Int
-    }
+  { mdl : Material.Model
+  , selectedTab : Int
+  }
 
 model : Model
 model =
-    { mdl = Material.model
-    , selectedTab = 0
-    }
+  { mdl = Material.model
+  , selectedTab = 0
+  }
 
 -- ACTION, UPDATE
 
 type Msg
-    = Mdl (Material.Msg Msg)
-    | SelectTab Int
+  = Mdl (Material.Msg Msg)
+  | SelectTab Int
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        Mdl msg' ->
-            Material.update msg' model
+  case msg of
+    Mdl msg' ->
+      Material.update msg' model
 
-        SelectTab num ->
-          { model | selectedTab = num } ! []
+    SelectTab num ->
+      { model | selectedTab = num } ! []
+
 
 -- VIEW
 
 type alias Mdl =
-    Material.Model
+  Material.Model
 
 view : Model -> Html Msg
 view model =
   Material.Scheme.topWithScheme Color.Teal Color.LightGreen <|
     Layout.render Mdl
-        model.mdl
-        [ Layout.fixedHeader
-        , Layout.onSelectTab SelectTab]
-        { header = [ h1 [ style [ ( "padding", "10px 0px 0px 10px" ), ( "margin", "0px" ) ] ] [ text "Budgeting Tool" ] ]
-        , drawer = []
-        , tabs = ( [text "Dashboard", text "Budget", text "Events", text "Challenges"], [] )
-        , main = [ viewBody model ]
-        }
+      model.mdl
+      [ Layout.fixedHeader
+      , Layout.onSelectTab SelectTab]
+      { header =
+        [ h1
+          [ style
+            [ ( "padding", "10px 0px 0px 10px" ), ( "margin", "0px" ) ] ]
+            [ text "Budgeting Tool" ]
+          ]
+      , drawer = []
+      , tabs =
+        ( [ text "Dashboard", text "Budget", text "Events", text "Challenges"]
+          , [] )
+      , main =
+        [ viewBody model ]
+      }
 
 viewBody : Model -> Html Msg
 viewBody model =
@@ -86,60 +95,88 @@ viewBody model =
     _ ->
       text "404"
 
-viewFilters : Model -> Html Msg
-viewFilters model =
-  Options.div [] [ text "Hey, look some filters!"]
-
 viewDash : Model -> Html Msg
 viewDash model =
-  grid [ Options.css "width" "100%"
-       , Options.css "padding" "0px"]
-      [ cell [ size All 12 ]
-             [ viewFilters model ]
-      , cell [ size All 12 ]
-             [ Card.view
-                [ Elevation.e4
-                , css "width" "100%"
-                , css "height" "100%"
-                , css "color" "white"
-                , Color.background (Color.color Color.Pink Color.S500)
-                ]
-                [ Card.title [] [ text "Titleholder"
-                                , Card.subhead [] [ text "Subheadholder" ] 
-                                ]
-                , Card.media [] [ Options.img [ Options.css "max-width" "100%" ] [ Html.Attributes.src "assets/images/ash.jpg" ] ]
-                , Card.text [ ] [ text "Textholder" ]
-                , Card.actions [ ] [ text "Actionholder" ]
-                ]
-              ]
+  grid
+    [ Options.css "width" "100%"
+    , Options.css "padding" "0px"]
+    [ cell
+      [ size All 12
+      , Options.css "width" "100%"
+      , Options.css "margin" "0px" ]
+      [ viewFilters model ]
+    , cell
+      [ size All 12 ]
+      [ Card.view
+        [ Elevation.e4
+        , css "width" "100%"
+        , css "height" "100%"
+        , css "color" "white"
+        , Color.background (Color.color Color.Pink Color.S500)
         ]
+      [ Card.title
+        []
+        [ text "Titleholder"
+        , Card.subhead
+          []
+          [ text "Subheadholder" ] 
+        ]
+      , Card.media
+        []
+        [ Options.img
+          [ Options.css "max-width" "100%" ]
+          [ Html.Attributes.src "assets/images/ash.jpg" ]
+        ]
+      , Card.text
+        []
+        [ text "Textholder" ]
+      , Card.actions
+        []
+        [ text "Actionholder" ]
+      ]
+    ]
+  ]
+
+viewFilters : Model -> Html Msg
+viewFilters model =
+  Options.div
+    [ Color.background ( Color.color Color.Teal Color.S50 )
+    , Options.css "padding" "5px" ]
+    [ text "Hey, look some filters!"]
+
 
 viewBudget : Model -> Html Msg
 viewBudget model =
-  grid [ Options.css "width" "100%"
-       , Options.css "padding" "0px" ]
-      [ cell [ size All 12
-              , Color.background (Color.color Color.DeepPurple Color.S50)
-              , Options.css "padding" "10px" ]
-              [ text "Budget"]
-      ]
+  grid
+    [ Options.css "width" "100%"
+    , Options.css "padding" "0px" ]
+    [ cell
+      [ size All 12
+      , Color.background (Color.color Color.DeepPurple Color.S50)
+      , Options.css "padding" "10px" ]
+      [ text "Budget"]
+    ]
 
 viewEvents : Model -> Html Msg
 viewEvents model =
-  grid [ Options.css "width" "100%"
-       , Options.css "padding" "0px" ]
-      [ cell [ size All 12
-              , Color.background (Color.color Color.DeepPurple Color.S50)
-              , Options.css "padding" "10px" ]
-              [ text "Events"]
-      ]
+  grid
+    [ Options.css "width" "100%"
+    , Options.css "padding" "0px" ]
+    [ cell
+      [ size All 12
+      , Color.background (Color.color Color.DeepPurple Color.S50)
+      , Options.css "padding" "10px" ]
+      [ text "Events"]
+    ]
 
 viewChallenges : Model -> Html Msg
 viewChallenges model =
-  grid [ Options.css "width" "100%"
-       , Options.css "padding" "0px" ]
-      [ cell [ size All 12
-              , Color.background (Color.color Color.DeepPurple Color.S50)
-              , Options.css "padding" "10px" ]
-              [ text "Challenges"]
-      ]
+  grid
+    [ Options.css "width" "100%"
+    , Options.css "padding" "0px" ]
+    [ cell
+      [ size All 12
+      , Color.background (Color.color Color.DeepPurple Color.S50)
+      , Options.css "padding" "10px" ]
+      [ text "Challenges"]
+    ]
