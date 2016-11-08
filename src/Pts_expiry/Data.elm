05abe0bd8c5ede1a861620ebe63b0e964b8134c7
Ashton Gabrielsen <ashton.gabrielsen@i28332-IS3.local>
2016-11-08 13:43:03 -0700

    refactored functions into separate files

---
+++ src/Pts_expiry/Data.elm
@@ -0,0 +1,31 @@
+module Pts_expiry.Data exposing (..)
+
+import Material
+
+import Set exposing (Set)
+
+import Mock_data
+
+type alias Model =
+  { mdl : Material.Model
+  , selected : Set Int
+  , selectedTab : Int
+  , data : List Mock_data.Munged_Data
+  }
+
+type Msg
+  = Mdl (Material.Msg Msg)
+  | SelectTab Int
+  | Toggle Int
+  | ToggleAll
+
+toggle : comparable -> Set comparable -> Set comparable
+toggle x set =
+  if Set.member x set then
+    Set.remove x set
+  else
+    Set.insert x set
+
+allSelected : Model -> Bool
+allSelected model =
+  Set.size model.selected == List.length model.data
